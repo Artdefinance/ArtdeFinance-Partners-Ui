3083ef429cf52522d69c2f4580da1ef15377186b
kino <66652533+kino-ona@users.noreply.github.com>
2022-08-30 18:04:40 +0900

    [pub] index path setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Footer from './Components/Layout/Footer';
 import Header from './Components/Layout/Header';
 import Guide from './Guide/Guide';
@@ -20,6 +20,65 @@ function App() {
           <Route path="/artist/regist" element={<Regist />} />
           <Route path="/gallery" element={<Gallery />} />
         </Routes>
+        <div className="pathGuide">
+          <table className="pathtable">
+            <thead>
+              <tr>
+                <th>depth1</th>
+                <th>depth2</th>
+                <th>path</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>component guide</td>
+                <td>-</td>
+                <td>
+                <Link to="/guide">
+                  /guide
+                </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Artist</td>
+                <td>Artist</td>
+                <td>
+                <Link to="/artist">
+                  /artist
+                </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>Artist List</td>
+                <td>
+                <Link to="/artist/list">
+                  /artist/list
+                </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>Artist Registration</td>
+                <td>
+                <Link to="/artist/regist">
+                  /artist/regist
+                </Link>
+                </td>
+              </tr>
+
+              <tr>
+                <td>Gallery</td>
+                <td>Gallery</td>
+                <td>
+                <Link to="/gallery">
+                  /gallery
+                </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         </div>
         <Footer />
       </BrowserRouter>
