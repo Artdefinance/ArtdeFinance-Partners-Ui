b2f55cbc103ac7904020879275b1f39fd40415f0
ona-hyejinkim <hyejin.kim@ona.co.kr>
2022-09-20 18:17:07 +0900

    img profile 관련 css 수정, curation > view 페이지 중간저장

---
+++ src/Components/StatusBar/StatusBar.js
@@ -0,0 +1,19 @@
+import './StatusBar.scss';
+import Slider from '@material-ui/core/Slider';
+
+export default function StatusBar() {
+  return (
+    <div>
+      <Slider
+        defaultValue={1}
+        step={1}
+        marks={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => ({
+          label: i,
+          value: i,
+        }))}
+        min={1}
+        max={10}
+      />
+    </div>
+  );
+}
