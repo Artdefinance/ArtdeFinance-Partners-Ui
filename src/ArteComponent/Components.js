1fd2f8165a9cabba2b17f3a4daea483b49a548f4
kino <66652533+kino-ona@users.noreply.github.com>
2022-10-17 16:19:45 +0900

    [pub] link error modi

--- src/ArteComponent/Components.js
+++ src/ArteComponent/Components.js
@@ -251,7 +251,6 @@ function ArteComponents() {
       <link
         rel="stylesheet"
         type="text/css"
-        charset="UTF-8"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
       />
       <link
